83a805e9c060e99712de3ba027c17208066ce849
Ravikantyadav18213 <raviyadav18213@gmail.com>
2025-12-02 11:29:00 +0000

    recipes2

--- my-app/src/components/Recipe.jsx
+++ my-app/src/components/Recipe.jsx
@@ -9,7 +9,7 @@ export default function Recipe()
 
 // save recipes to localstorage
 const saveRecipes=(updatedRecipes)=>{
-    localStorage.setItem("recipes",JSON.stringify(updatedRecipes);)
+    localStorage.setItem("recipes",JSON.stringify(updatedRecipes));
 }
 // Load recipes
 
@@ -40,20 +40,20 @@ const handleSubmit=(e)=>{
         <>
         <div style={{maxWidth:"600px",margin:"20px auto",border:"1px solid #ccc",padding:"30px"}}>
             <h1 style={{marginBottom:"20px",border:"1px solid #ccc",padding:"20px"}}>Recipe Book</h1>
-            <form>
+            <form onSubmit={handleSubmit}>
             <div style={{marginBottom:"10px"}}>
                 <label>Name:</label> <br />
-                <input type="text" style={{width:"100%",padding:"5px"}} />
+                <input type="text" style={{width:"100%",padding:"5px"}} value={recipename} onChange={(e)=>setRecipeName(e.target.value)} />
             </div>
               
                <div style={{marginBottom:"10px"}}>
                 <label>Ingredients:</label> <br />
-                <input type="text" style={{width:"100%",padding:"5px"}} />
+                <input type="text" style={{width:"100%",padding:"5px"}} value={Ingredients} onChange={(e)=>setIngredients(e.target.value)}/>
             </div>
 
              <div style={{marginBottom:"10px"}}>
                 <label>Instruction:</label> <br />
-                <input type="text" style={{width:"100%",padding:"5px"}} />
+                <input type="text" style={{width:"100%",padding:"5px"}}value={Instruction} onChange={(e)=>setInstruction(e.target.value)} />
             </div>
 
             <button style={{padding:"5px 10px"}} type="submit">Add Recipe </button>
