3a6eccb8e6c37cb2de33b30f2d1433f17aa7c09b
Ravikantyadav18213 <raviyadav18213@gmail.com>
2025-12-03 10:22:38 +0000

    recipe5

--- my-app/src/components/Recipe.jsx
+++ my-app/src/components/Recipe.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 export default function Recipe()
 {
@@ -6,12 +6,18 @@ export default function Recipe()
     const[Ingredients,setIngredients]=useState("");
     const[Instruction,setInstruction]=useState("");
     const [recipes,setRecipes]=useState([]);
+    const [editingId,setEditingId]=useState(null);
 
 // save recipes to localstorage
 const saveRecipes=(updatedRecipes)=>{
     localStorage.setItem("recipes",JSON.stringify(updatedRecipes));
+    setRecipes(updatedRecipes);
 }
 // Load recipes
+useEffect(()=>{
+    const storedRecipes=JSON.parse(localStorage.getItem("recipes"))||[]
+    setRecipes(storedRecipes);
+},[])
 
 
 // Add recipes
@@ -21,6 +27,14 @@ const handleSubmit=(e)=>{
     {
         alert("please fill all fields");
         return;
+    }
+    if(editingId)
+    {
+        const updatedRecipes=recipes.map((r)=>
+        r.id===editingId?{...r,name:recipename,Ingredients,Instruction}:r
+    )
+    saveRecipes(updatedRecipes);
+    setEditingId(null)
     }
     else{
         const newRecipe={
@@ -35,11 +49,26 @@ const handleSubmit=(e)=>{
     setIngredients("")
     setInstruction("")
 }
+// Delete Recipe
+const handleDelete=(id)=>{
+    const updatedRecipes=recipes.filter((r)=>r.id!==id)
+    saveRecipes(updatedRecipes);
+}
+
+// edit recipe
+
+const handleEdit=(r)=>{
+    setRecipeName(r.name);
+    setIngredients(r.Ingredients);
+    setInstruction(r.Ingredients);
+    setEditingId(r.id)
+}
+
 
     return(
         <>
-        <div style={{maxWidth:"600px",margin:"20px auto",border:"1px solid #ccc",padding:"30px"}}>
-            <h1 style={{marginBottom:"20px",border:"1px solid #ccc",padding:"20px"}}>Recipe Book</h1>
+        <div style={{maxWidth:"600px",margin:"20px auto",border:"1px solid #ccc",padding:"30px", background:"yellow"}}>
+            <h1 style={{marginBottom:"20px",border:"1px solid #ccc",padding:"20px",background:"lightgreen"}}>Recipe Book</h1>
             <form onSubmit={handleSubmit}>
             <div style={{marginBottom:"10px"}}>
                 <label>Name:</label> <br />
@@ -56,10 +85,27 @@ const handleSubmit=(e)=>{
                 <input type="text" style={{width:"100%",padding:"5px"}}value={Instruction} onChange={(e)=>setInstruction(e.target.value)} />
             </div>
 
-            <button style={{padding:"5px 10px"}} type="submit">Add Recipe </button>
+            <button style={{padding:"5px 10px"}} type="submit">
+                {editingId ?"Update Recipe":"Add Recipe"} </button>
 
           </form>
         </div>
+         <div style={{maxWidth:"600px",margin:"20px auto",border:"1px solid #ccc",padding:"30px", background:"orange"}}>
+            <h3>All Recipes</h3>
+            {recipes.length===0 && <p>no recipas added yet</p>}
+            {recipes.map((r)=>(
+               <div key={r.id} style={{border:"1px solid #ccc",padding:"10px",marginBottom:"10px",color:"white"}}>
+               <h4>Recipe Name:{r.name}</h4>
+               <p><strong>Ingredients</strong></p>
+               <ul>{r.Ingredients}</ul>
+               <p><strong>Instruction:{r.Instruction}</strong></p>
+               <button style={{padding:"3px 8px",marginRight:"5px"}} onClick={()=>handleEdit(r)}>Edit</button>
+               <button style={{padding:"3px 8px"}} onClick={()=>handleDelete(r.id)}>Delete</button>
+
+               </div>
+
+            ))}
+         </div>
         
         </>
     )
